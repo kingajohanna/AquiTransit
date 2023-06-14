21d3b8e7ec068bc2e2710109e201d9898fcc16dd
kolja <kolja.eggers@tuhh.de>
2023-06-14 19:14:47 +0200

    made new branch

--- frontend/src/navigation/RootNavigator.tsx
+++ frontend/src/navigation/RootNavigator.tsx
@@ -1,6 +1,7 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { HomeScreen } from "../screens/HomeScreen";
 import { SettingsScreen } from "../screens/SettingsScreen";
+import { FavoritesScreen } from "../screens/FavoritesScreen";
 import Icon from "react-native-vector-icons/Ionicons";
 
 const Tab = createMaterialBottomTabNavigator();
@@ -22,9 +23,19 @@ export const RootNavigator = () => {
         name="Settings"
         component={SettingsScreen}
         options={{
-          tabBarLabel: "Home",
+          tabBarLabel: "Settings",
           tabBarIcon: ({ color }) => (
             <Icon name="settings" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          tabBarLabel: "Favorites",
+          tabBarIcon: ({ color }) => (
+            <Icon name="heart" color={color} size={26} />
           ),
         }}
       />
---
+++ frontend/src/theme/colors.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+    auth_stack: '#aee5a5',
+    bottom_top_bar: '#467e3d',
+    main_stack: '#d1efcc',
+    active_icon: '#24fc01',
+    non_active_icon: '#1ccb00',
+    auth_stack_title: '#000000',
+    auth_stack_text: '#6b6b6b',
+    white: '#ffffff',
+    stop_line_title: '#1e5615',
+    bus_departure_time: '#565656',
+};
